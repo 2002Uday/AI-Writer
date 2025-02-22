c563a7bcd9a52b5e53db03c90d104d067464f9a2
Uday <udaybharadwa2002@gmail.com>
2025-02-22 21:11:45 +0530

    Fixing 404 Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar"; // You'll need to create this
 import Home from "./pages/Home"; // You'll need to create this
-import BrainstormingSession from "./pages/BrainstormingSession";
+import BrainstormingSession from "./pages/BrainstormingSession"; // You'll need to create this
 import { ProtectedRoute, AuthRoute } from "./middleware/auth";
 import Login from "./pages/Login"; // You'll need to create this
 import Register from "./pages/Register"; // You'll need to create this
@@ -16,19 +16,17 @@ function App() {
       <Routes>
         {/* Public routes */}
         <Route path="/" element={<Home />} />
-
         {/* Auth routes (accessible only when not logged in) */}
         <Route element={<AuthRoute />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Route>
-
-         {/* Protected routes (accessible only when logged in) */}
-         {/* <Route element={<ProtectedRoute />}> */}
+        {/* Protected routes (accessible only when logged in) */}
+        <Route element={<ProtectedRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/brainstorm" element={<BrainstormingSession />} />
           {/* Add other protected routes here */}
-        {/* </Route> */}
+        </Route>
       </Routes>
     </>
   );
